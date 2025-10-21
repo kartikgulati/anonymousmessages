aaa64152b6b2a936663f5c5787e9f2f7d45d2f1b
kartikgulati <kartikgulati19@gmail.com>
2025-10-20 23:38:51 -0400

    Setup resend email for the verifcation of the users

--- src/helpers/sendVerificationEmail.ts
+++ src/helpers/sendVerificationEmail.ts
@@ -1,2 +1,30 @@
 import { resend } from "@/lib/resend";
 
+import VerificationEmail from "../../emails/VerificationEmail";
+
+import { ApiResponse } from "@/types/ApiResponse";
+
+export async function sendVerficationEmail(
+  email: string,
+  username: string,
+  verifyCode: string
+): Promise<ApiResponse> {
+  try {
+    await resend.emails.send({
+    from: 'Acme <onboarding@resend.dev>',
+    to: email,
+    subject: 'Verfication Code Email',
+    react: VerificationEmail({ username, otp:verifyCode }),
+  });
+    return {
+      success: true,
+      message: "message sent successfully",
+    };
+  } catch (emailError) {
+    console.log("Error sending verfication emails:", emailError);
+    return {
+      success: false,
+      message: "failed to send the email, please try againa later",
+    };
+  }
+}
