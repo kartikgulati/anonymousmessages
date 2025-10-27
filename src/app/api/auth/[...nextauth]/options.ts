a37c5370105a03311f9dfd158c1b2d3e0710bb76
kartikgulati <kartikgulati19@gmail.com>
2025-10-26 22:35:40 -0400

    added middleware and next auth functionality

---
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,81 @@
+import CredentialsProvider from "next-auth/providers/credentials";
+import { NextAuthOptions } from "next-auth";
+import bcrypt from "bcryptjs";
+import UserModel from "@/model/User";
+import dbConnect from "@/lib/dbConnect";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      id: "credentials",
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "text "},
+      password: { label: "Password", type: "password" },
+      },
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      async authorize(credentials:  any ): Promise<any>{
+        await dbConnect();
+
+        try {
+            const user = await UserModel.findOne({
+                $or:[
+                    {email: credentials.identifier},
+                    {username: credentials.identifier}
+                ]
+            })
+            if(!user){
+                throw new Error("No user found with the given email and user name")
+            }
+
+            if (!user.isVerified) {
+                throw new Error("please verify your email and account first")
+            }
+
+            const isPasswordValid  = await bcrypt.compare(credentials.password, user.password)
+
+            if(!isPasswordValid){
+                throw new Error ("Invalid credentials")
+            }else{
+                return user
+            }
+
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        } catch (err:any) {
+            throw new Error(err)
+            
+        }
+      }
+    }),
+  ],
+  callbacks: {
+    async session({ session, token }) {
+        if (token) {
+            session.user._id = token._id as string;
+            session.user.isVerified = token.isVerified as boolean;
+            session.user.isAcceptingMessage = token.isAcceptingMessage as boolean;
+            session.user.username = token.username as string;
+            
+        }
+      return session
+    },
+    async jwt({ token, user}) {
+        if (user) {
+            token._id = user._id?.toString();
+            token.isVerified = user.isVerified;
+            token.isAcceptingMessage = user.isAcceptingMessage;
+            token.username = user.username;
+
+        }
+      return token
+    },
+  },
+  pages: {
+    signIn: '/sign-in',
+  },
+  session:{
+    strategy: 'jwt',
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+
+};
