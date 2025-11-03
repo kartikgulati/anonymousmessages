287e50c5d05f1e522407578d7aed9705e96b5bea
kartikgulati <kartikgulati19@gmail.com>
2025-11-03 11:41:44 -0500

    adding message api via aggregation pipeline

---
+++ src/app/api/accept-messages/route.ts
@@ -0,0 +1,8 @@
+import {getServerSession} from "next-auth/next";
+import {authOptions} from "../auth/[...nextauth]/route";
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+import {User} from "next-auth";
+
+
+
