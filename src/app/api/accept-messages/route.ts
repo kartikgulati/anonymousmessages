2ba194f8d2651d954d084e1ab1b90368feff5a9e
kartikgulati <kartikgulati19@gmail.com>
2025-11-04 12:05:15 -0500

    fixing routes

--- src/app/api/accept-messages/route.ts
+++ src/app/api/accept-messages/route.ts
@@ -3,6 +3,24 @@ import {authOptions} from "../auth/[...nextauth]/route";
 import dbConnect from "@/lib/dbConnect";
 import UserModel from "@/model/User";
 import {User} from "next-auth";
+import { success } from "zod/mini";
 
 
+export async function POST(request: Request) {
+    dbConnect();
+    
+    const session = await getServerSession(authOptions);
+    const user: User = session?.user 
 
+    if(!session || !session.user) {
+        return Response.json(
+        {success: false,
+            message: "verfication failed"
+        },
+        {status: 401}
+    )
+    }
+
+   const userId = user._id
+
+}
